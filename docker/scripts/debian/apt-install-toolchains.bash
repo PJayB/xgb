484197ae432c060232d0ae07aa03c1c8cdc48d32
Pete Lewis <mygit@pjblewis.com>
2024-11-03 05:41:45 -0500

    Skip native arch when installing cross-compilers

--- docker/scripts/debian/apt-install-toolchains.bash
+++ docker/scripts/debian/apt-install-toolchains.bash
@@ -12,13 +12,16 @@ packages=(
 )
 
 # Install foreign architecture toolchains
+native_arch="$(dpkg --print-architecture)"
 while read -r arch ; do
+    [ "$native_arch" == "$arch" ] && continue
+
     arch="$(echo "$arch" |
         sed 's/amd64/x86_64/g' |
         sed 's/arm64/aarch64/g' |
         sed 's/i386/i686/g' |
         cat)"
-    packages+=( "g++-$arch-linux-gnu" )
+    packages+=( "g++-$arch-linux-gnu:$native_arch" )
 done < <(dpkg --print-foreign-architectures)
 
 apt-get install -y "${packages[@]}"
